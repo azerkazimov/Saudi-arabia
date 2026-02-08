66aee57180b077aa9bce3c30a3c978722aa9f1ca
Azer Kazimov <azer.kazimov@yahoo.com>
2026-02-08 10:26:38 +0400

    Saudi Arabia

---
+++ app/destenations/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/ui/hero";
+
+export default function Destenations() {
+    return (
+        <div>
+            <Hero
+                image="/destenations.png"
+                title="Destenations"
+                description="Saudi Arabia is rich in heritage and history. The country is 
+home to hundreds of historically important sites." />
+        </div>
+    )
+}
